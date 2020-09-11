8f9c8ae82ac31d80753c347aed5dc6feb1ebd3ca
Vitalii Bonar <vitaliib@tytocare.com>
2020-09-11 11:09:56 +0300

    Issue 486A

---
+++ 486A_calculating_function/486A_calculating_function.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <fstream>
+#include <algorithm>
+#include <cmath>
+
+using namespace std;
+
+bool DEBUG = false;
+
+ifstream fileStream;
+
+istream &getInputStream()
+{
+    if (DEBUG) {
+        string path = __FILE__;                             
+        path = path.substr(0, 1 + path.find_last_of('/'));
+        path += "input.txt";
+        fileStream.open(path);
+        return fileStream;
+    }
+    return cin;
+}
+
+void solve()
+{
+    istream &inputStream = getInputStream();
+
+    // Your code goes here
+
+    long long int n;
+    inputStream >> n;
+    cout << n / 2 - n * (n & 1) << endl;
+}
+
+int main(int argc, char *argv[])
+{
+    if (argc >= 2) {
+        string debug(argv[1]);
+        if (debug == "debug") {
+            DEBUG = true;
+        }
+    }
+    solve();
+    return 0;
+}
+
