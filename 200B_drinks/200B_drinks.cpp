bbbab1ee957c784f92096dff3d0c30fb9e470e31
Vitalii Bonar <vitaliib@tytocare.com>
2020-09-11 15:52:00 +0300

    Issue 200B

---
+++ 200B_drinks/200B_drinks.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <fstream>
+#include <algorithm>
+
+using namespace std;
+
+bool DEBUG = false;
+
+ifstream fileStream;
+
+istream &getInputStream()
+{
+    if (DEBUG) {
+        string path = __FILE__;                             
+        path = path.substr(0, 1 + path.find_last_of('/'));
+        path += "input.txt";
+        fileStream.open(path);
+        return fileStream;
+    }
+    return cin;
+}
+
+void solve()
+{
+    istream &inputStream = getInputStream();
+
+    // Your code goes here
+
+    int n, p, sum = 0;
+    inputStream >> n;
+    
+    for (int i(0); i < n; i++) {
+        inputStream >> p;
+        sum += p;
+    }
+    cout << sum / (double) n << endl;
+}
+
+int main(int argc, char *argv[])
+{
+    if (argc >= 2) {
+        string debug(argv[1]);
+        if (debug == "debug") {
+            DEBUG = true;
+        }
+    }
+    solve();
+    return 0;
+}
+
